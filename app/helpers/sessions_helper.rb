e9ddea0208dffcda1d1dafd90dd039399634150e
evgenii <silaewgenii@gmail.com>
2022-12-03 22:38:51 +0300

    Implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,12 +3,27 @@ module SessionsHelper
     def log_in(user)
         session[:user_id] = user.id
     end
+
+        # запоминает позьзователя в саенсе
+    def remember(user)
+        user.remember
+        cookies.permanent.encrypted[:user_id] = user.id
+        cookies.permanent[:remember_token] = user.remember_token
+    end
+
     #возвращает авторизировавшегося пользователя
     def current_user
-        if session[:user_id]
-            @current_user ||= User.find_by(id: session[:user_id])
-        end
+        if (user_id = session[:user_id])
+            @current_user ||= User.find_by(id: user_id)
+        elsif (user_id = cookies.encrypted[:user_id])
+            user = User.find_by(id: user_id)
+            if user && user.authenticated?(cookies[:remember_token])
+                log_in user
+                @current_user = user
+            end
+        end     
     end
+    
     #возвращает тру если пользователь зашёл
     def logged_in?
         !current_user.nil?
@@ -19,5 +34,20 @@ module SessionsHelper
         session.delete(:user_id)
         @current_user = nil
     end
+
+    # забывает текущий сеанс
+    def forget(user)
+        user.forget
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
+    end
+    
+    #выход текущим пользователем
+    def log_out
+        forget(current_user)
+        session.delete(:user_id)
+        @current_user = nil
+    end
+
 
 end
